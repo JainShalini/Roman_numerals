7fc8c471b2f2a7c91b808804a6170cd8947452fc
Navin Karra <navinkumar.karra@sky.uk>
2026-08-27 16:08:17 +0100

    added a  comment  about the use of fill constructor

--- src/romanConverter.h
+++ src/romanConverter.h
@@ -2,5 +2,6 @@
 
 std::string convertIntToRoman(int numToConvert)
 {
+    // constructor of std::String to create a string with character repeated n times
     return std::string(numToConvert,'I');
 }
