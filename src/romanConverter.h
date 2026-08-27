b91928e644cf3b7ebbfc9ac9c1e57b204d187913
Navin Karra <navinkumar.karra@sky.uk>
2026-08-27 16:01:29 +0100

    refactored to remove duplication

--- src/romanConverter.h
+++ src/romanConverter.h
@@ -2,10 +2,5 @@
 
 std::string convertIntToRoman(int numToConvert)
 {
-    if (numToConvert == 1)
-        return "I";
-    else if (numToConvert == 2)
-        return "II";
-    
-    return "III";
+    return std::string(numToConvert,'I');
 }
