c7d0f3574e2d787adffab1dff38166aa9f61a36b
JainShalini <shalini.jain@sky.uk>
2026-09-08 15:57:24 +0100

    extracted the handing of 10 to separate function appendRomanNumeral

--- src/romanConverter.h
+++ src/romanConverter.h
@@ -4,33 +4,32 @@ std::string repeatCharacter(int &numToConvert) {
   return std::string(numToConvert, 'I');
 }
 
+void appendRomanNumeral(int &numToConvert, std::string &roman) {
+  if (numToConvert >= 10) {
+    roman += "X";
+    numToConvert -= 10;
+  }
+}
 std::string convertIntToRoman(int numToConvert) {
 
-    std::string roman("");
+  std::string roman("");
 
-    if (numToConvert >= 10)
-    {
-        roman += "X";
-        numToConvert -= 10;
-    }
+  appendRomanNumeral(numToConvert, roman);
 
-    if (numToConvert >= 9)
-    {
-      roman += "IX";
-      numToConvert -= 9;
-    }
+  if (numToConvert >= 9) {
+    roman += "IX";
+    numToConvert -= 9;
+  }
 
-    if (numToConvert >= 5)
-    {
-       roman += "V";
-       numToConvert = numToConvert - 5; 
-    }
+  if (numToConvert >= 5) {
+    roman += "V";
+    numToConvert = numToConvert - 5;
+  }
 
-    if (numToConvert >= 4)
-    {
-        roman += "IV";
-        numToConvert = numToConvert - 4;
-    }
+  if (numToConvert >= 4) {
+    roman += "IV";
+    numToConvert = numToConvert - 4;
+  }
 
-    return roman+= repeatCharacter(numToConvert);
+  return roman += repeatCharacter(numToConvert);
 }
