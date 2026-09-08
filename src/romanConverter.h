116ffd1efe6fbb54006356d6404a8a73395eeb21
JainShalini <shalini.jain@sky.uk>
2026-09-08 14:21:43 +0100

    added a newline

--- src/romanConverter.h
+++ src/romanConverter.h
@@ -3,6 +3,7 @@
 std::string repeatCharacter(int &numToConvert) {
   return std::string(numToConvert, 'I');
 }
+
 std::string convertIntToRoman(int numToConvert) {
   if (numToConvert == 4)
     return ("IV");
