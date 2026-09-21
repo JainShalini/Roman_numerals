4b4487207b15de795576a0bb0e20e03be8a93eaf
JainShalini <shalini.jain@sky.uk>
2026-09-21 18:08:29 +0100

    passed till 1000

--- src/romanConverter.h
+++ src/romanConverter.h
@@ -12,6 +12,8 @@ std::string convertIntToRoman(int numToConvert) {
 
   std::string roman("");
 
+  appendRomanNumeral(numToConvert, roman, 1000, "M");
+
   appendRomanNumeral(numToConvert, roman, 900, "CM");
 
   appendRomanNumeral(numToConvert, roman, 500, "D");
--- test/test_roman_numerals.cpp
+++ test/test_roman_numerals.cpp
@@ -27,5 +27,6 @@ INSTANTIATE_TEST_SUITE_P(, RomanNumeralTest, testing::Values(
     std::make_pair(100, "C"),
     std::make_pair(400, "CD"),
     std::make_pair(500, "D"),
-    std::make_pair(900, "CM")
+    std::make_pair(900, "CM"),
+    std::make_pair(1000, "M")
 ));
